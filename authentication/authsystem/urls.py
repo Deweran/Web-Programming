8239fd829cd35a60ed2194c97d9eda65580bfdfd
Deweran <Deweran@github.com>
2022-10-18 20:51:34 +0900

    complete

--- authentication/authsystem/urls.py
+++ authentication/authsystem/urls.py
@@ -4,6 +4,7 @@ from . import views
 #app_name = 'auth'
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
