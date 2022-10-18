cb5e4f3d9ecc700dacc6add3a9ccf371d6dccf83
Deweran <Deweran@github.com>
2022-10-18 20:39:52 +0900

    building

--- authentication/authsystem/urls.py
+++ authentication/authsystem/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 from . import views
 
-app_name = 'auth'
+#app_name = 'auth'
 
 urlpatterns = [
-    
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
